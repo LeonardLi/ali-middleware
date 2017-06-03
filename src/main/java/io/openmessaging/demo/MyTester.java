d0f2fa76d1f404404928a537185cbdd81c3580ac
xiaode <liyangde66@gmail.com>
2017-06-03 23:41:37 +0800

    使用NIO写的test

--- src/main/java/io/openmessaging/demo/MyTester.java
+++ src/main/java/io/openmessaging/demo/MyTester.java
@@ -6,35 +6,48 @@ import io.openmessaging.Message;
 import io.openmessaging.tester.Constants;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.rmi.server.ExportException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by xiaode on 6/2/17.
  */
 public class MyTester {
 
-    public static void main(String[] args){
-        DefaultBytesMessage message = new DefaultBytesMessage("Test".getBytes());
-//        message.putHeaders("amount",10);
-//        message.putProperties("data","ted");
-//        message.setBody("hhhhhhhh".getBytes());
-        message.putHeaders("Queue","QUEUE_0");
-        message.setBody("PRODUCER_7_1".getBytes());
-        //Queue:QUEUE_0,;;PRODUCER_7_1
-        String result = messageToString(message);
-        Message message1 = StringToMessage(result);
-        String[] results = result.split(";");
-
-        try{
-            Class kvClass = Class.forName("io.openmessaging.demo.DefaultKeyValue");
-            KeyValue keyValue = (KeyValue) kvClass.newInstance();
-            keyValue.put("STORE_PATH", Constants.STORE_PATH);
-            DefaultPullConsumer consumer = new DefaultPullConsumer(keyValue);
-
-        }catch (Exception e){
-            e.printStackTrace();
+    public static void main(String[] args) throws InterruptedException{
+        CountDownLatch latch = new CountDownLatch(100);
+        TestConcurrentWrite[] tests = new TestConcurrentWrite[100];
+        for (TestConcurrentWrite test: tests) {
+            test = new TestConcurrentWrite("A",latch);
+            Thread thread = new Thread(test);
+            thread.start();
+            thread.join();
         }
+        //latch.await();
 
-
+        //Thread.sleep(1000);
+        TestConcurrentWrite.flush();
+//        try {
+//            RandomAccessFile aFile = new RandomAccessFile(Constants.STORE_PATH + "/test2", "rw");
+//            FileChannel fc = aFile.getChannel();
+//            String newData = "New String to write to file..." + System.currentTimeMillis();
+//            ByteBuffer buf = ByteBuffer.allocate(48);
+//            buf.clear();
+//            buf.put(newData.getBytes());
+//            buf.flip();
+//            while (buf.hasRemaining()) {
+//                fc.write(buf);
+//            }
+//        } catch(Exception e){
+//            e.printStackTrace();
+//        }
 
     }
 
@@ -81,5 +94,42 @@ public class MyTester {
         }
         if(segments[2]!= null && segments.length!=0) message.setBody(segments[2].getBytes());
        return message;
+    }
+
+
+}
+class TestConcurrentWrite implements Runnable {
+    private ThreadLocal<String> tags = new ThreadLocal<>();
+    CountDownLatch latch;
+    public TestConcurrentWrite(String tag,CountDownLatch latch) {
+        tags.set(tag);
+        this.latch = latch;
+    }
+    static ByteBuffer buffer = ByteBuffer.allocate(10240);
+
+
+    @Override
+    public void run() {
+        //super.run();
+
+            String newData = "New String to write to file..."+Thread.currentThread()+":"+System.currentTimeMillis()+"\n";
+            buffer.put(newData.getBytes());
+            latch.countDown();
+    }
+
+    static void flush(){
+        try {
+            File file = new File(Constants.STORE_PATH + "/test3");
+
+            FileOutputStream fos = new FileOutputStream(file,true);
+            FileChannel fc = fos.getChannel();
+            buffer.flip();
+            while(buffer.hasRemaining()){
+                fc.write(buffer);
+            }
+            buffer.clear();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
